d744c62f0a436b8b26231c0a0c3f64fa6d153417
Worawat Poompipat <worawat.pom@techinvent.co.th>
2021-09-28 17:15:30 +0700

    2

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app_name'));
+app.use(express.static(__dirname + '/dist/test-deeplinking'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/app_name/index.html'));
+res.sendFile(path.join(__dirname+'/dist/test-deeplinking/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
